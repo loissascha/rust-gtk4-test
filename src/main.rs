3c6edce120a5d510704c2fa16ced080a3342cd52
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-25 19:49:54 +0200

    fstab

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
+use fstab::FsEntry;
+use fstab::FsTab;
 use gtk::Application;
 use gtk::prelude::*;
+use std::path::Path;
 
-mod fstab;
+// mod fstab;
 mod ui;
 
 fn main() {
-    let ftab = fstab::Fstab::new();
+    // let ftab = fstab::Fstab::new();
 
     // println!("---- found fstab ----");
     // println!("{}", ftab.content);
     // println!("---------------------");
+    readfstab();
 
     let app = Application::builder()
         .application_id("at.sascha.GtkTest")
@@ -21,4 +25,19 @@ fn main() {
     });
 
     app.run();
+}
+
+fn readfstab() {
+    let fstab = FsTab::new(Path::new("/etc/fstab"));
+    let entries = fstab.get_entries().unwrap();
+
+    for entry in entries {
+        println!("Device: {}", entry.fs_spec);
+        println!("Mountpoint: {:?}", entry.mountpoint);
+        println!("Filesystem: {}", entry.vfs_type);
+        println!("Options: {:?}", entry.mount_options);
+        println!("Dump: {}", entry.dump);
+        println!("Fsck order: {}", entry.fsck_order);
+        println!("---");
+    }
 }
