3f826c9f7526ca3513a18c54f292b171fed6929b
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-25 13:55:01 +0200

    ui

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,6 @@
 use gtk::{
-    Application, ApplicationWindow, Box, Button, HeaderBar, ListBox, ListBoxRow, MenuButton,
-    Orientation, Stack, gio,
+    AboutDialog, Application, ApplicationWindow, Box, Button, HeaderBar, ListBox, ListBoxRow,
+    MenuButton, Orientation, Stack, gio,
 };
 use gtk::{Label, prelude::*};
 
@@ -132,6 +132,39 @@ pub fn build_ui(app: &Application) {
         .child(&hlayout)
         .build();
     window.set_titlebar(Some(&header_bar));
+
+    let window_clone = window.clone();
+
+    let preferences_action = gio::SimpleAction::new("preferences", None);
+    preferences_action.connect_activate(move |_, _| {
+        let dialog = gtk::Window::builder()
+            .title("Preferences")
+            .default_width(400)
+            .default_height(300)
+            .transient_for(&window_clone)
+            .modal(true)
+            .build();
+
+        dialog.set_child(Some(&gtk::Label::new(Some("Preferences go here"))));
+        dialog.present();
+    });
+    app.add_action(&preferences_action);
+
+    let window_clone = window.clone();
+
+    let about_action = gio::SimpleAction::new("about", None);
+    about_action.connect_activate(move |_, _| {
+        let dialog = AboutDialog::builder()
+            .program_name("GTK Rust Test")
+            .version("0.1.0")
+            .comments("A small GTK app written in Rust.")
+            .website("https://sascha-loishandl.at")
+            .transient_for(&window_clone)
+            .modal(true)
+            .build();
+        dialog.present();
+    });
+    app.add_action(&about_action);
 
     window.present();
 }
